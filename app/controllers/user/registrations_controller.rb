af90f151ecaf8e5c55ae9811c65107069eed3e5b
Nikola Miljkovic <nikomil@gmail.com>
2018-10-30 12:52:59 -0500

    Added profile controller

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -1,13 +1,8 @@
 # frozen_string_literal: true
 
 class User::RegistrationsController < Devise::RegistrationsController
-  ALLOWED_FIELDS = %w(first_name last_name email country city
-      state zip sex date_of_birth marital_status education
-      weekly_hours sleep_hours commute_hours exercise_hours diet
-      present_roles future_roles role_category)
-
   before_action :configure_sign_up_params, only: [:create]
-  before_action :configure_account_update_params, only: [:update]
+  # before_action :configure_account_update_params, only: [:update]
 
   # GET /users/sign_up
   # def new
@@ -25,50 +20,50 @@ class User::RegistrationsController < Devise::RegistrationsController
   # end
 
   # PUT /users
-  def update
-    # TODO: All temporary; Make this more railsy and keep DRY
-    if update_params.include? :present_roles
-      current_user.roles.where(tense: :present).destroy_all
-      update_params[:present_roles].split.each do |label|
-        role = Role.find_or_create_by! label: label
-        current_user.roles.create!({ role_id: role.id, tense: :present })
-      end
-      update_params.delete :present_roles
-    end
-    if update_params.include? :future_roles
-      current_user.roles.where(tense: :future).destroy_all
-      update_params[:future_roles].split.each do |label|
-        role = Role.find_or_create_by! label: label
-        current_user.roles.create!({ role_id: role.id, tense: :future })
-      end
-      update_params.delete :future_roles
-    end
-    if update_params.include? :role_category
-      update_params[:role_category].each do |label,importance|
-        role = current_user.role_categories.find_or_initialize_by(role_category_id: label)
-        role.importance = importance
-        role.save!
-      end
-      update_params.delete :role_category
-    end
-    if update_params.include? :goal
-      update_params[:goal].each do |term,goals|
-        goals.each do |category,text|
-          next unless text.present?
-          role_category = RoleCategory.find category
-          goal = current_user.goals.find_or_initialize_by term: term, role_category: role_category
-          goal.text = text
-          goal.save!
-        end
-      end
-      # TODO: Handle last step properly
-      update_params.delete :goal
-      current_user.onboarded_at = Time.now
-      current_user.save!
-    end
-    current_user.update_attributes! update_params
-    render json: { msg: 'okay!' }
-  end
+  # def update
+  #   # TODO: All temporary; Make this more railsy and keep DRY
+  #   if update_params.include? :present_roles
+  #     current_user.roles.where(tense: :present).destroy_all
+  #     update_params[:present_roles].split.each do |label|
+  #       role = Role.find_or_create_by! label: label
+  #       current_user.roles.create!({ role_id: role.id, tense: :present })
+  #     end
+  #     update_params.delete :present_roles
+  #   end
+  #   if update_params.include? :future_roles
+  #     current_user.roles.where(tense: :future).destroy_all
+  #     update_params[:future_roles].split.each do |label|
+  #       role = Role.find_or_create_by! label: label
+  #       current_user.roles.create!({ role_id: role.id, tense: :future })
+  #     end
+  #     update_params.delete :future_roles
+  #   end
+  #   if update_params.include? :role_category
+  #     update_params[:role_category].each do |label,importance|
+  #       role = current_user.role_categories.find_or_initialize_by(role_category_id: label)
+  #       role.importance = importance
+  #       role.save!
+  #     end
+  #     update_params.delete :role_category
+  #   end
+  #   if update_params.include? :goal
+  #     update_params[:goal].each do |term,goals|
+  #       goals.each do |category,text|
+  #         next unless text.present?
+  #         role_category = RoleCategory.find category
+  #         goal = current_user.goals.find_or_initialize_by term: term, role_category: role_category
+  #         goal.text = text
+  #         goal.save!
+  #       end
+  #     end
+  #     # TODO: Handle last step properly
+  #     update_params.delete :goal
+  #     current_user.onboarded_at = Time.now
+  #     current_user.save!
+  #   end
+  #   current_user.update_attributes! update_params
+  #   render json: { msg: 'okay!' }
+  # end
 
   # DELETE /users
   # def destroy
@@ -84,11 +79,11 @@ class User::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  protected
-
-  def update_params
-    @update_params ||= params.require(:user).permit(*ALLOWED_FIELDS, { role_category: {}, goal: {} })
-  end
+  # protected
+  #
+  # def update_params
+  #   @update_params ||= params.require(:user).permit(*ALLOWED_FIELDS, { role_category: {}, goal: {} })
+  # end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
@@ -96,9 +91,9 @@ class User::RegistrationsController < Devise::RegistrationsController
   end
 
   # If you have extra params to permit, append them to the sanitizer.
-  def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: ALLOWED_FIELDS)
-  end
+  # def configure_account_update_params
+  #   devise_parameter_sanitizer.permit(:account_update, keys: ALLOWED_FIELDS)
+  # end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, module: :user
   get '/', to: 'dashboard#index', as: 'dashboard'
+  get '/profile', to: 'profile#index'
+  put '/profile', to: 'profile#update'
 end
