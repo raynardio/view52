735662e7f91d89a97f089b0c22805b7aafb7aab5
Nikola Miljkovic <nikomil@gmail.com>
2018-10-30 15:26:56 -0500

    Added time chart

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -13,6 +13,28 @@ class DashboardController < ApplicationController
         end
       end
       @role_categories = current_user.role_categories.order(importance: :desc)
+
+      @time_spent_labels = [
+        t(:sleeping),
+        t(:commuting),
+        t(:exercising),
+        t(:working),
+        t(:other)
+      ]
+      other_hours = 24.0 - (
+        current_user.sleep_hours +
+        current_user.commute_hours +
+        current_user.exercise_hours +
+        (current_user.weekly_hours / 7.0)
+      )
+      @time_spent_values = [
+          current_user.sleep_hours,
+          current_user.commute_hours,
+          current_user.exercise_hours,
+          (current_user.weekly_hours / 7.0),
+          other_hours
+      ]
+
     else
       redirect_to profile_path
     end
