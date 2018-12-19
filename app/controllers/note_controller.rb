e8cac73860c9ea3296fa64ca227fc5d72b23b649
Nikola Miljkovic <nikomil@gmail.com>
2018-12-18 23:14:04 -0600

    Added edit note

--- app/controllers/note_controller.rb
+++ app/controllers/note_controller.rb
@@ -35,7 +35,39 @@ class NoteController < ApplicationController
     render json: { error: false }
   end
 
+  def update
+    note = current_user.notes.find params[:id]
+    params.delete :id
+
+    tags = params[:tags] && params[:tags].split || []
+    views = params[:views] && params[:views].split || []
+
+    ActiveRecord::Base.transaction do
+      note.tags.destroy_all
+      note.views.destroy_all
+      note.update_attributes!({
+        text: permitted_params[:note],
+        title: permitted_params[:title]
+      })
+      tags.each do |tag|
+        note.tags.create!({user_id: current_user.id, label: tag})
+      end
+
+      views.each do |view|
+        note.views.create!({user_id: current_user.id, label: view})
+      end
+    end
+
+    render json: note
+  end
+
   def show
     @note = current_user.notes.find params[:id]
+  end
+
+  private
+
+  def permitted_params
+    params.permit :title, :note
   end
 end
