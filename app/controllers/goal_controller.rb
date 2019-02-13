43f93b3133724910c47b3b0f537e48ef7ce32f38
Nikola Miljkovic <nikomil@gmail.com>
2019-02-12 19:25:42 -0600

    Implemented goal modal

--- app/controllers/goal_controller.rb
+++ app/controllers/goal_controller.rb
@@ -28,8 +28,49 @@ class GoalController < ApplicationController
     render json: goal
   end
 
+  def update
+    goal = current_user.goals.find params[:id]
+    tags = params[:tags] && params[:tags].split || []
+    views = [params[:view]]
+
+    ActiveRecord::Base.transaction do
+      goal.tags.destroy_all
+      goal.views.destroy_all
+
+      tags.each do |tag|
+        goal.tags.create!({user_id: current_user.id, label: tag})
+      end
+
+      views.each do |view|
+        next if view.nil?
+        goal.views.create!({user_id: current_user.id, label: view})
+      end
+
+      goal.update_attributes!({
+        text: permitted_params[:title],
+        term: permitted_params[:term],
+        goal_date: (Date.strptime(permitted_params[:goal_date], '%m/%d/%Y') rescue nil),
+        description: permitted_params[:description]
+      })
+      goal.save!
+    end
+
+    render json: goal
+  end
+
   def show
     goal = current_user.goals.find params[:id]
     render partial: 'modal/modal_goal', locals: { goal: goal }
+  end
+
+  def destroy
+    goal = current_user.goals.find params[:id]
+    render json: { result: goal.destroy }
+  end
+
+  private
+
+  def permitted_params
+    params.permit :title, :description, :goal_date, :term
   end
 end
