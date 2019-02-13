ddebac10b8aa997cb2e144256d194fcd5aa303b1
Nikola Miljkovic <nikomil@gmail.com>
2019-02-12 21:16:41 -0600

    Life event update working

--- app/controllers/life_event_controller.rb
+++ app/controllers/life_event_controller.rb
@@ -30,6 +30,38 @@ class LifeEventController < ApplicationController
     render json: event
   end
 
+  def update
+    life_event = current_user.life_events.find params[:id]
+
+    ActiveRecord::Base.transaction do
+      life_event.views.destroy_all
+      life_event.web_links.destroy_all
+
+      unless params[:view].blank?
+        life_event.views.create!({
+          user_id: current_user.id,
+          label: params[:view]
+        })
+      end
+
+      unless params[:secondary_view].blank?
+        life_event.views.create!({
+          user_id: current_user.id,
+          label: params[:secondary_view]
+        })
+      end
+
+      unless params[:url].blank?
+        WebLink.create_from_url! params[:url], current_user, life_event
+      end
+
+      life_event.update_attributes! permitted_params
+      life_event.save!
+    end
+
+    render json: { life_event: life_event }
+  end
+
   def show
     life_event = current_user.life_events.find params[:id]
     render partial: 'modal/modal_life_event', locals: { life_event: life_event }
@@ -38,5 +70,11 @@ class LifeEventController < ApplicationController
   def destroy
     life_event = current_user.life_events.find params[:id]
     render json: { result: life_event.destroy }
+  end
+
+  private
+
+  def permitted_params
+    params.permit :title, :from_date, :to_date, :prototype, :show_in_annual_view, :description
   end
 end
